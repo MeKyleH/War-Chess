3b681b04d3451de7e5020e03fc8cf6526b02897a
meKyleh <kyle.a.holt@gmail.com>
2016-05-14 18:43:26 -0600

    Major refactor to focus on player movements

---
+++ Assets/Scripts/Knight.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Knight : Chessman {
+
+	public override bool[,] PossibleMove () {
+		bool[,] r = new bool[8, 8];
+
+		//Up Left
+		KinghtMove(CurrentX -1, CurrentY +2, ref r);
+
+		//Left Up
+		KinghtMove(CurrentX -2, CurrentY +1, ref r);
+
+		//Left Down
+		KinghtMove(CurrentX -2, CurrentY -1, ref r);
+
+		//Down Left
+		KinghtMove(CurrentX -1, CurrentY -2, ref r);
+
+		//Down Right
+		KinghtMove(CurrentX +1, CurrentY -2, ref r);
+
+		//Right Down
+		KinghtMove(CurrentX +2, CurrentY -1, ref r);
+
+		//Right Up
+		KinghtMove(CurrentX +2, CurrentY +1, ref r);
+
+		//Up Right
+		KinghtMove(CurrentX +1, CurrentY +2, ref r);
+
+		return r;
+	}
+
+	public void KinghtMove(int x, int y, ref bool[,] r) {
+		Chessman c;
+		if (x >= 0 && x < 8 && y >= 0 && y < 8) {
+			c = BoardManager.Instance.Chessmans [x, y];	
+			if (c == null) {
+				r [x, y] = true;
+			} else if (isWhite != c.isWhite) {
+				r [x, y] = true;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Pawn.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pawn : Chessman {
+
+
+	public override bool[,] PossibleMove() {
+		bool[,] r = new bool[8,8];
+
+		Chessman c, c2;
+		int[] e = BoardManager.Instance.EnPassantMove;
+
+		//White team move
+		if (isWhite) {
+			//DIAGONAL LEFT
+			//enpassant
+			if (e [0] == CurrentX - 1 && e [1] == CurrentY + 1) {
+				r [CurrentX - 1, CurrentY + 1] = true;
+			}
+			if (CurrentX != 0 && CurrentY != 7) {
+				c = BoardManager.Instance.Chessmans [CurrentX - 1, CurrentY + 1];
+				if (c != null && !c.isWhite) {
+					r [CurrentX - 1, CurrentY + 1] = true;
+				}
+			}
+			//DIAGONAL RIGHT
+			//enpassant
+			if (e [0] == CurrentX + 1 && e [1] == CurrentY + 1) {
+				r [CurrentX + 1, CurrentY + 1] = true;
+			}
+			if (CurrentX != 7 && CurrentY != 7) {
+				c = BoardManager.Instance.Chessmans [CurrentX + 1, CurrentY + 1];
+				if (c != null && !c.isWhite) {
+					r [CurrentX + 1, CurrentY + 1] = true;
+				}
+			}
+			//MIDDLE
+			if (CurrentY != 7) {
+				c = BoardManager.Instance.Chessmans [CurrentX, CurrentY + 1];
+				if (c == null)
+					r [CurrentX, CurrentY + 1] = true;
+			}
+			//MIDDLE ON FIRST MOVE
+			if (CurrentY == 1) {
+				c = BoardManager.Instance.Chessmans [CurrentX, CurrentY + 1];
+				c2 = BoardManager.Instance.Chessmans [CurrentX, CurrentY + 2];
+				if (c == null && c2 == null) {
+					r [CurrentX, CurrentY + 2] = true;
+				}
+			}
+
+
+		//Black team move
+		} 
+		else {
+			//DIAGONAL LEFT
+			//enpassant
+			if (e [0] == CurrentX - 1 && e [1] == CurrentY - 1) {
+				r [CurrentX - 1, CurrentY - 1] = true;
+			}
+			if (CurrentX != 0 && CurrentY != 0) {
+				c = BoardManager.Instance.Chessmans [CurrentX - 1, CurrentY - 1];
+				if (c != null && c.isWhite) {
+					r [CurrentX - 1, CurrentY - 1] = true;
+				}
+			}
+			//DIAGONAL RIGHT
+			//enpassant
+			if (e [0] == CurrentX + 1 && e [1] == CurrentY - 1) {
+				r [CurrentX + 1, CurrentY - 1] = true;
+			}
+			if (CurrentX != 7 && CurrentY != 0) {
+				c = BoardManager.Instance.Chessmans [CurrentX + 1, CurrentY - 1];
+				if (c != null && c.isWhite) {
+					r [CurrentX + 1, CurrentY - 1] = true;
+				}
+			}
+			//MIDDLE
+			if (CurrentY != 0) {
+				c = BoardManager.Instance.Chessmans [CurrentX, CurrentY - 1];
+				if (c == null)
+					r [CurrentX, CurrentY - 1] = true;
+			}
+			//MIDDLE ON FIRST MOVE
+			if (CurrentY == 6) {
+				c = BoardManager.Instance.Chessmans [CurrentX, CurrentY - 1];
+				c2 = BoardManager.Instance.Chessmans [CurrentX, CurrentY - 2];
+				if (c == null && c2 == null) {
+					r [CurrentX, CurrentY - 2] = true;
+				}
+			}
+		}
+
+		return r;
+
+	}
+}
