628a5c2233345b0d21e3e411812144345bab855c
meKyleh <kyle.a.holt@gmail.com>
2016-05-14 19:25:58 -0600

    updating buying menu

---
+++ Assets/Scripts/Defender.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Defender : MonoBehaviour {
+
+	public int goldCost = 1;
+
+	private GoldDisplay goldDisplay;
+
+	void Start() {
+		goldDisplay = GameObject.FindObjectOfType<GoldDisplay> ();
+	}
+
+	public void AddGold(int amount) {
+		goldDisplay.AddGold (amount);
+	}
+}
---
+++ Assets/Scripts/TurnManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnManager : MonoBehaviour {
+
+	public GameObject buyButton;
+	public GameObject buyMenu;
+	public GameObject takeGoldButton;
+	public GoldDisplay goldDisplay;
+	public GameObject moveButton;
+
+	public void OpenBuyMenu() {
+		buyMenu.SetActive (true);
+		moveButton.SetActive (true);
+		buyButton.SetActive (false);
+	}
+
+	public void MovePiece() {
+		buyButton.SetActive (true);
+		buyMenu.SetActive (false);
+		moveButton.SetActive (false);
+	}
+
+	public void TakeGold() {
+		goldDisplay.AddGold (1);
+		EndTurn ();
+	}
+
+	public void EndTurn() {
+		//TODO WRITE ENDTURN METHOD
+		Debug.Log("Turn Ended");
+	}
+}
