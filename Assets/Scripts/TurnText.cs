0f668a59e0d0c276750972c9e05b829aef72eebd
meKyleh <kyle.a.holt@gmail.com>
2016-05-15 22:05:27 -0600

    add turn notification

---
+++ Assets/Scripts/TurnText.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TurnText : MonoBehaviour {
+
+	private Text turnText;
+	private BoardManager boardManager;
+	private bool isWhiteTurn = true;
+
+	private void Start () {
+		turnText = GetComponent<Text> ();
+		boardManager = GameObject.FindObjectOfType<BoardManager>();
+	}
+	
+	private void Update () {
+		if (boardManager.isWhiteTurn != isWhiteTurn) {
+			isWhiteTurn = boardManager.isWhiteTurn;
+			UpdateText ();
+		}
+	}
+	private void UpdateText() {
+		turnText.text = isWhiteTurn ? "White turn" : "Black turn";
+	}
+}
