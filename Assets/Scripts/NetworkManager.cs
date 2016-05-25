69ee9667cfe3e164f8087c6ab191d8dfbc84796e
meKyleh <kyle.a.holt@gmail.com>
2016-05-24 23:07:28 -0600

    updating turn text to sync across users

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 using ExitGames.Client.Photon;
@@ -9,34 +9,62 @@ public class NetworkManager : MonoBehaviour {
 	[SerializeField] Text teamColorText;
 	[SerializeField] Transform[] spawnPoints;
 	[SerializeField] Camera sceneCamera;
-	[SerializeField] string roomName = "Kyle";
+	[SerializeField] GameObject serverWindow; //TODO RENAME TO LOBBYWINDOW
+	[SerializeField] InputField username;
+	[SerializeField] InputField roomName;
+	[SerializeField] Text turnText;
 
 	GameObject player;
+	PhotonView photonView;
 
 	private const string VERSION = "0.2";
 	private ExitGames.Client.Photon.Hashtable customProps;
 	private BoardManager boardManager;
+	public bool isWhiteTurn; //TODO MAKE PRIVATE
 
 	void Start () {
 		PhotonNetwork.logLevel = PhotonLogLevel.Full;
 		PhotonNetwork.ConnectUsingSettings (VERSION);
 		PhotonNetwork.autoCleanUpPlayerObjects = false;
 
+		photonView = GetComponent<PhotonView> ();
+		StartCoroutine ("UpdateConnectionString");
+
 		boardManager = GameObject.FindObjectOfType<BoardManager> ();
+		isWhiteTurn = boardManager.isWhiteTurn;
 	}
 
-	void Update() {
-		connectionText.text = PhotonNetwork.connectionStateDetailed.ToString ();
+	// displays the connection status string at the bottom to the player
+	IEnumerator UpdateConnectionString() {
+		while (true) {
+			connectionText.text = PhotonNetwork.connectionStateDetailed.ToString ();
+			yield return null;
+		}
 	}
 
+
+	//activates the window for joining games
 	void OnJoinedLobby() {
+		serverWindow.SetActive (true);
+	}
+
+	//called by the button when a player has entered their username and room
+	public void JoinRoom() {
+		PhotonNetwork.player.name = username.text;
 		RoomOptions ro = new RoomOptions () {isVisible = true, maxPlayers = 2};
-		PhotonNetwork.JoinOrCreateRoom (roomName, ro, TypedLobby.Default);
+		PhotonNetwork.JoinOrCreateRoom (roomName.text, ro, TypedLobby.Default);
 		customProps = new ExitGames.Client.Photon.Hashtable ();
 	}
 
+
 	//On initial room Join spawn the player immediately
 	void OnJoinedRoom() {
+		serverWindow.SetActive (false);
+		StopCoroutine ("UpdateConnectionString");
+		connectionText.text = "";
+		turnText.text = this.isWhiteTurn ? "White Turn" : "Black Turn";
+
+		//set player teams
 		if (PhotonNetwork.isMasterClient) {
 			customProps.Add ("isWhiteTeam", true);
 		} else {
@@ -50,8 +78,6 @@ public class NetworkManager : MonoBehaviour {
 
 		if (boardManager.Chessmans[0,0] == null) {
 			Debug.Log ("FOUND CHESSMANS!!!!!!!!!!!!!!!");
-
-			boardManager.SpawnAllChessmans ();
 		}
 	}
 
@@ -73,6 +99,20 @@ public class NetworkManager : MonoBehaviour {
 			spawnPoints [index].rotation,
 			0);
 		sceneCamera.enabled = false;
+	}
+
+	void Update() {
+		if (boardManager.isWhiteTurn != isWhiteTurn) {
+			photonView.RPC ("EndTurn_RPC", PhotonTargets.All, !isWhiteTurn);
+		}
+	}
+
+	//used for syncing player turns
+	[PunRPC]
+	void EndTurn_RPC(bool isWhiteTurn) {
+		this.isWhiteTurn = isWhiteTurn;
+		boardManager.isWhiteTurn = isWhiteTurn;
+		turnText.text = this.isWhiteTurn ? "White Turn" : "Black Turn";
 	}
 
 }
